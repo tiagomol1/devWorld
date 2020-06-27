ec020d84a68d11f18b4ab4abfe21f0f26228e211
Tiago Murilo <62064267+tiagomol1@users.noreply.github.com>
2020-06-27 16:03:04 -0300

    Pagina ajustada com layout da timeline e interação com Javascript feita.

--- pages/main.php
+++ pages/main.php
@@ -10,6 +10,8 @@
     <script src="https://kit.fontawesome.com/b5207eb6cb.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../styles/header.css">
     <link rel="stylesheet" href="../styles/global.css">
+    <link rel="stylesheet" href="../styles/timeline.css">
+    <script src="../js/interaction.js"></script>
     <title>DevWorld</title>
   </head>
   <body>
@@ -21,7 +23,10 @@
       </div>
     </div>  
     <div class="menuSpaceBetweenImage">
-      <img src="../assets/images/logo.png" alt="" width="80px">
+      <a href="main.php">
+        <img src="../assets/images/logo.png" alt="" width="80px">
+        <p>DevWorld</p>
+      </a>
     </div>
     <div class="menuSpaceBetweenNav">
       <nav>
@@ -29,7 +34,7 @@
         <a href="#"><i class="fas fa-briefcase"></i></a>
         <a href="#"><i class="fas fa-comments"></i></a>
         <a href="#"><i class="fas fa-file-code"></i></a>
-        <a href="#"><i class="fas fa-user iconUser"></i></a>
+        <a href="#"><img src="../assets/images/tiago.png" alt="" class="imagePerfil"></a>
       </nav>
     </div>
   </header>
@@ -37,15 +42,92 @@
   <container class="containerBody">
 
     <content class="leftBody">
-
+      <fieldset class="fieldsetVacancies">
+        <legend>Vagas</legend>
+      </fieldset>
+      <fieldset class="fieldsetBirthdays">
+        <legend>Aniversariantes</legend>
+      </fieldset>
+      <fieldset class="fieldsetRank">
+        <legend>Rank da Semana</legend>
+      </fieldset>
     </content>
 
     <content class="centerBody">
+      <!-- Area publicação -->
+      <form class="publishingArea" id="publishingArea" action="post">
+        <textarea name="textPublication" id="textPublication" onInput="sizeTexPublicationArea()" cols="80" rows="10" placeholder="Escreva alguma coisa..."></textarea>
+        <div class="publishingOptions">
+          <div>
+            <i class="far fa-image"></i>
+            <i class="fas fa-video"></i>
+            <i class="fas fa-paperclip"></i>
+          </div>
+          <button type="submit">Publicar</button>
+        </div>
+      </form>
+
+      <div class="publications">
+        <div class="headerPublication">
+          <img src="../assets/images/mauricio.jpg" alt="" class="imagePerfil">
+          <div>
+            <p class="publicationAuthor">Mauricio Nunes</p>
+            <p class="publicationDate">27/06/2020 às 19:00 hrs</p>
+          </div>
+        </div>
+        <div class="bodyPublication">
+          <div class="publicationText">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit corrupti et hic odit? Necessitatibus, iure? Itaque praesentium accusamus reprehenderit voluptas autem, voluptatum non dolorem suscipit mollitia facere! Saepe, est. Fugit.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit corrupti et hic odit? Necessitatibus, iure? Itaque praesentium accusamus reprehenderit voluptas autem, voluptatum non dolorem suscipit mollitia facere! Saepe, est. Fugit.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit corrupti et hic odit? Necessitatibus, iure? Itaque praesentium accusamus reprehenderit voluptas autem, voluptatum non dolorem suscipit mollitia facere! Saepe, est. Fugit.</p>
+          </div>
+          <div class="publicationImages">
+            
+          </div>
+          <hr />
+          <div class="publicationReaction">
+            <i class="fas fa-comment"><p>4</p></i>
+            <i class="fas fa-heart"><p>5</p></i>
+            <i class="fas fa-thumbs-up"><p>12</p></i>
+          </div>
+        </div>
+      </div>
+
+      <div class="publications">
+        <div class="headerPublication">
+          <img src="../assets/images/tiago.png" alt="" class="imagePerfil">
+          <div>
+            <p class="publicationAuthor">Tiago Murilo</p>
+            <p class="publicationDate">27/06/2020 às 12:00 hrs</p>
+          </div>
+        </div>
+        <div class="bodyPublication">
+          <div class="publicationText">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit corrupti et hic odit? Necessitatibus, iure? Itaque praesentium accusamus reprehenderit voluptas autem, voluptatum non dolorem suscipit mollitia facere! Saepe, est. Fugit.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit corrupti et hic odit? Necessitatibus, iure? Itaque praesentium accusamus reprehenderit voluptas autem, voluptatum non dolorem suscipit mollitia facere! Saepe, est. Fugit.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit corrupti et hic odit? Necessitatibus, iure? Itaque praesentium accusamus reprehenderit voluptas autem, voluptatum non dolorem suscipit mollitia facere! Saepe, est. Fugit.</p>
+          </div>
+          <div class="publicationImages">
+            <img src="" alt="">
+          </div>
+          <hr />
+          <div class="publicationReaction">
+            <i class="fas fa-comment"><p>2</p></i>
+            <i class="fas fa-heart"><p>5</p></i>
+            <i class="fas fa-thumbs-up"><p>10</p></i>
+          </div>
+        </div>
+      </div>
 
     </content>
 
     <content class="rightBody">
-
+      <fieldset class="fieldsetProjects">
+        <legend>Projetos</legend>
+      </fieldset>
+      <fieldset class="fieldsetCommunities">
+        <legend>Comunidades</legend>
+      </fieldset>
     </content>
 
   </container>
